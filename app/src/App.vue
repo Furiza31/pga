329021f5114cb32eb3fab812f7f01dd8396be377
Furiza31 <hugo.wendjaneh@gmail.com>
2025-05-01 21:03:37 +0200

    feat(app)

---
+++ app/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <router-view />
+  <div class="h-10 w-10"></div>
+</template>
+
+<script setup lang="ts">
+// Root component that renders the current route matched component
+</script>
